76b515ce1f522404ad5eed2d48614251bd520e3e
Tellisa-Ione <emefa.16@outlook.com>
2025-10-19 21:58:18 +0000

    update: created test api

---
+++ TestAPI/TestAPI/Controllers/BakeryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BakeryController : Controller
+    {
+        [HttpPost("tea-bread")]
+        public string TeaBread() {
+
+            return "TeaBread!!";
+        
+        }
+    }
+}
